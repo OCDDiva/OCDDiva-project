b03d01cfbeb073efbb9c0496f2d25c7e7d495318
Stephen Vertucci <steve.vertucci@gmail.com>
2023-06-24 12:47:31 -0400

    started conditional questions for ultimate service

--- src/components/CustomerForm/CleaningQuestions.jsx
+++ src/components/CustomerForm/CleaningQuestions.jsx
@@ -1,32 +1,82 @@
-//Imports go here
 import React, { useState } from 'react';
 
 function CleaningQuestions() {
-    //Code goes here
-    const [selectedOption, setSelectedOption] = useState('');
+  const [selectedOption, setSelectedOption] = useState('');
 
-    const optionChoice = (e) => {
-        setSelectionOption(e.target.value);
-    }
+  const optionChoice = (e) => {
+    setSelectedOption(e.target.value);
+  };
 
-    //What displays
-    return (
-        <>
-        Steve: Test 
-        <br />
-        <br />
+  return (
+    <>
+      Steve: Test
+      <br />
+      <br />
+      <div>
         <form>
-            <label>
-                Select an Option:
-                <select value={selectedOption} onChange={optionChoice}>
-                    <option value="">Please select</option>
-
-                </select>
-            </label>
+          <label>
+            Select an Option:
+            <select value={selectedOption} onChange={optionChoice}>
+              <option value="">Please select</option>
+              <option value="ultimate">ULTIMATE</option>
+              <option value="essential">ESSENTIAL</option>
+            </select>
+          </label>
         </form>
+      </div>
+      {selectedOption === "ultimate" && (
+        <>
+          <h2>Ultimate Service Questions</h2>
+          <label>
+            # of Bedrooms:
+            <input type="number" min="0" />
+          </label>
+          <label>
+            # of Bathrooms:
+            <input type="number" min="0" />
+          </label>
+          <label>
+            # of Additional Rooms:
+            <input type="number" min="0" />
+          </label>
+          <label>
+            # of Doors/Windows to be cleaned:
+            <input type="number" min="0" />
+          </label>
+          <label>
+            D0you have pets?
+            <input type="number" min="0" />
+          </label>
+          <label>
+            Hazardous conditions? (ex mold, mildew, bugs, rats)
+            <input type="number" min="0" />
+          </label>
         </>
+      )}
+      {selectedOption === "essential" && (
+        <>
+          <h2>Essential Service Questions</h2>
+          {/* Add your Essential Service questions here */}
+        </>
+      )}
+    </>
+  );
+}// End Inquiries()
 
-    )
-} // End Inquiries()
+// THIS IS MADE AS A BACKUP FORM FOR TESTING PURPOSES OR TO BE USED LATER 
+ {/* <h2>Cleaning Services</h2> 
+                <p>Please Select a cleaning service:</p>
+
+                <div class="essential">
+                    <h3>Essential</h3>
+                    <p>Description of Essential</p>
+                    <button>Select Essential</button>
+                </div>
+
+                <div class="ultimate">
+                    <h3>Ultimate</h3>
+                    <p>Description of Ultimate</p>
+                    <button>Select Ultimate</button>
+                </div> */}
 
 export default CleaningQuestions;
