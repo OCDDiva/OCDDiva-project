b09e8192f771b1b63c0de4a31ae4aff5beeb7a11
Stephen Vertucci <steve.vertucci@gmail.com>
2023-06-24 13:30:40 -0400

    form set, creating states for each questions to maintain value throughout the form

--- src/components/CustomerForm/CleaningQuestions.jsx
+++ src/components/CustomerForm/CleaningQuestions.jsx
@@ -1,70 +1,140 @@
 import React, { useState } from 'react';
 
 function CleaningQuestions() {
-  const [selectedOption, setSelectedOption] = useState('');
+    const [selectedOption, setSelectedOption] = useState('');
 
-  const optionChoice = (e) => {
-    setSelectedOption(e.target.value);
-  };
+    // Setting state for each questions
+    const [numberBedrooms, setNumberBedrooms] = useState(0);
 
-  return (
-    <>
-      Steve: Test
-      <br />
-      <br />
-      <div>
-        <form>
-          <label>
-            Select an Option:
-            <select value={selectedOption} onChange={optionChoice}>
-              <option value="">Please select</option>
-              <option value="ultimate">ULTIMATE</option>
-              <option value="essential">ESSENTIAL</option>
-            </select>
-          </label>
-        </form>
-      </div>
-      {selectedOption === "ultimate" && (
+    const optionChoice = (e) => {
+        setSelectedOption(e.target.value);
+    };
+
+    const nextStep = (event) => {
+        event.preventDefault();
+
+    }
+
+    return (
         <>
-          <h2>Ultimate Service Questions</h2>
-          <label>
-            # of Bedrooms:
-            <input type="number" min="0" />
-          </label>
-          <label>
-            # of Bathrooms:
-            <input type="number" min="0" />
-          </label>
-          <label>
-            # of Additional Rooms:
-            <input type="number" min="0" />
-          </label>
-          <label>
-            # of Doors/Windows to be cleaned:
-            <input type="number" min="0" />
-          </label>
-          <label>
-            D0you have pets?
-            <input type="number" min="0" />
-          </label>
-          <label>
-            Hazardous conditions? (ex mold, mildew, bugs, rats)
-            <input type="number" min="0" />
-          </label>
+            Steve: Test
+            <br />
+            <br />
+            <div>
+                <form>
+                    <label>
+                        Select an Option:
+                        <select value={selectedOption} onChange={optionChoice} >
+                            <option value="">Please select</option>
+                            <option value="ultimate">ULTIMATE</option>
+                            <option value="essential">ESSENTIAL</option>
+                        </select>
+                    </label>
+                </form>
+            </div>
+            {selectedOption === "ultimate" && (
+                <>
+                    <h2>Ultimate Service Questions</h2>
+                    <ol>
+                        <li>
+                            <label>
+                                # of Bedrooms:
+                                <input type="number" min="0" />
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                # of Bathrooms:
+                                <input type="number" min="0" />
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                # of Additional Rooms:
+                                <input type="number" min="0" />
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                # of Doors/Windows to be cleaned:
+                                <input type="number" min="0" />
+                            </label>
+                        </li>
+                        <li>
+                            <label>Do you have pets</label>
+                            <label>
+                                <input type="radio" name="pets" value="yes" />
+                                Yes
+                            </label>
+                            <label>
+                                <input type="radio" name="pets" value="no" />
+                                No
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                Hazardous conditions? ex mold, mildew, bugs, rats
+                                <input type="text" />
+                            </label>
+                        </li>
+                    </ol>
+                    <button  onClick={nextStep}>Next</button>
+                </>
+            )}
+            {selectedOption === "essential" && (
+                <>
+                    <h2>Essential Service Questions</h2>
+                    <ol>
+                        <li>
+                            <label>
+                                # Bedrooms:
+                                <input type="number" min="0" />
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                # of Bathrooms:
+                                <input type="number" min="0" />
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                # of Additional Rooms:
+                                <input type="number" min="0" />
+                            </label>
+                        </li>
+
+                        <li>
+                            <label>
+                                # of Doors/Windows to be cleaned:
+                                <input type="number" min="0" />
+                            </label>
+                        </li>
+                        <li>
+                            <label>Do you have pets</label>
+                            <label>
+                                <input type="radio" name="pets" value="yes" />
+                                Yes
+                            </label>
+                            <label>
+                                <input type="radio" name="pets" value="no" />
+                                No
+                            </label>
+                        </li>
+                    </ol>
+                    <button  onClick={nextStep}>Next</button>
+                </>
+            )}
         </>
-      )}
-      {selectedOption === "essential" && (
-        <>
-          <h2>Essential Service Questions</h2>
-          {/* Add your Essential Service questions here */}
-        </>
-      )}
-    </>
-  );
+    );
 }// End Inquiries()
 
+
+
+
+
 // THIS IS MADE AS A BACKUP FORM FOR TESTING PURPOSES OR TO BE USED LATER 
- {/* <h2>Cleaning Services</h2> 
+{/* <h2>Cleaning Services</h2> 
                 <p>Please Select a cleaning service:</p>
 
                 <div class="essential">
