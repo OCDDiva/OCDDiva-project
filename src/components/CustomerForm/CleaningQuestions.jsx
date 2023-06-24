a19f91abd3f339f601cde1bbbc1e4987ae37c316
Stephen Vertucci <steve.vertucci@gmail.com>
2023-06-24 11:52:49 -0400

    created state for cleaning options, and form with dropdown option menu

--- src/components/CustomerForm/CleaningQuestions.jsx
+++ src/components/CustomerForm/CleaningQuestions.jsx
@@ -1,12 +1,29 @@
 //Imports go here
+import React, { useState } from 'react';
 
 function CleaningQuestions() {
     //Code goes here
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const optionChoice = (e) => {
+        setSelectionOption(e.target.value);
+    }
 
     //What displays
     return (
         <>
+        Steve: Test 
+        <br />
+        <br />
+        <form>
+            <label>
+                Select an Option:
+                <select value={selectedOption} onChange={optionChoice}>
+                    <option value="">Please select</option>
 
+                </select>
+            </label>
+        </form>
         </>
 
     )
