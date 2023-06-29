d481b9e644de46b4b4e99c5a136cf452625393bf
MIguel Torres <mtorres913@gmail.com>
2023-06-28 20:43:39 -0500

    created history reducer and imported to root.reducer file

--- src/redux/reducers/history.reducer.js
+++ src/redux/reducers/history.reducer.js
@@ -1,1 +1,10 @@
 
+const history = (state = [], action) => {
+    switch (action.type){
+        case 'SET_HISTORY':
+        return action.payload;
+        default:
+            return state;
+    }
+}
+export default history;
