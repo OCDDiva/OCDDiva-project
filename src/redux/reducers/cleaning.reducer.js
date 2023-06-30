def7b6c88183e18b8daad78b008f86fe08b3f77a
Stephen Vertucci <steve.vertucci@gmail.com>
2023-06-29 20:19:30 -0400

    cleaned up cleaning reducer

--- src/redux/reducers/cleaning.reducer.js
+++ src/redux/reducers/cleaning.reducer.js
@@ -1,13 +1,4 @@
-// const initialState = {
-//     numBedrooms: 0,
-//     numBathrooms: 0,
-//     numAdditionalRooms: 0,
-//     numDoorsWindows: 0,
-//     hasPets: '',
-//     hazardousConditions: '',
-//   };
-  
-  const cleaningQuestionsReducer = (state = {}, action) => {
+const cleaningQuestionsReducer = (state = {}, action) => {
     switch (action.type) {
       case 'SET_CLEANING_QUESTIONS':
         console.log('SET_CLEANING_Q dispatched')
