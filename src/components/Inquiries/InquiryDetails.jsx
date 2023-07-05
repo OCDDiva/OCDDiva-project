0705a32bdae59ae52b8ed0d88cc5fff0f1133b81
Everett Butler <everettjamesbutler@gmail.com>
2023-07-04 22:16:50 -0500

    Got edit/add note button to display, now have to set up the backend

--- src/components/Inquiries/InquiryDetails.jsx
+++ src/components/Inquiries/InquiryDetails.jsx
@@ -14,10 +14,29 @@ function InquiryDetails() {
         dispatch({ type: 'FETCH_INQUIRY_DETAILS', payload: inquiriesId });
     }, [inquiriesId]);
 
+    const [note, setNote] = useState('');
+
     console.log(inquiriesId);
 
     const returnToInquiries = (event) => {
         history.push('/inquiries')
+    }
+
+    const changeNote = (e) => {
+        e.preventDefault();
+        if (note.length > 0) {
+            dispatch({ type: 'EDIT_NOTE', payload: { note, inquiriesId, }, history});
+        } else {
+            dispatch({ type: 'ADD_NOTE', payload: { note }, history})
+        }
+    }
+
+    const noteButton = () => {
+        if (note.length > 0) {
+            return 'Edit Note'
+        } else {
+            return 'Add Note'
+        }
     }
 
     //What displays
@@ -34,9 +53,13 @@ function InquiryDetails() {
                         Organizing: {JSON.stringify(inquiryDetails.Organizing)},
                         Decluttering: {JSON.stringify(inquiryDetails.Declutter)} 
                     </h2>
+                    <h3>Date Received: do we need this for incoming inquiries? or only completed customers?{inquiryDetails.service_on} </h3>
                     <h3> {inquiryDetails.completion_status}</h3>
                     {/* TODO I know the priority is bugged, will work on a fix */}
                     <h4> {inquiryDetails.priority}</h4>
+                    <h2>NOTES: (insert notes here)
+                    <button onClick={changeNote}>{noteButton()}</button>
+                    </h2>
                     <button onClick={returnToInquiries}>Inquiries List</button>
                 </div>
         </main>
