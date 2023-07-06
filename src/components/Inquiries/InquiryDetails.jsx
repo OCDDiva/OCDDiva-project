311c73957db8a62e5dab2bfbfa96308b8a926463
Everett Butler <everettjamesbutler@gmail.com>
2023-07-06 15:33:26 -0500

    got displays to be conditional on details page. Setting up put routes for notes

--- src/components/Inquiries/InquiryDetails.jsx
+++ src/components/Inquiries/InquiryDetails.jsx
@@ -16,21 +16,22 @@ function InquiryDetails() {
 
     console.log(inquiriesId);
 
+
     const returnToInquiries = (event) => {
         history.push('/inquiries')
     }
 
     const changeNote = (e) => {
         e.preventDefault();
-        if (inquiryDetails.notes.length > 0) {
-            dispatch({ type: 'EDIT_NOTE', payload: { notes, inquiriesId, }, history});
+        if (inquiryDetails.notes > 0) {
+            dispatch({ type: 'EDIT_NOTE', payload: { notes, inquiriesId, }, history });
         } else {
-            dispatch({ type: 'ADD_NOTE', payload: { notes }, history})
+            dispatch({ type: 'ADD_NOTE', payload: { notes }, history })
         }
     }
 
     const noteButton = () => {
-        if (inquiryDetails.notes.length > 0) {
+        if (inquiryDetails.notes > 0) {
             return 'Edit Note'
         } else {
             return 'Add Note'
@@ -82,46 +83,76 @@ function InquiryDetails() {
         return `${date}`
     }
 
+    const cleaningDisplay = (inquiryDetails) => {
+        if (inquiryDetails.Cleaning === true) {
+            return <div>
+                <h5>Cleaning Questions:</h5>
+                <p>Number of Doors & Windows: {inquiryDetails.DoorsWindows}</p>
+                <p>Has Pets? {petConversion(inquiryDetails)}</p>
+                <p>Hazardous Conditions? {inquiryDetails.HazardousConditions}</p>
+            </div>
+        } else {
+            return ''
+        }
+    }
+
+    const movingDisplay = (inquiryDetails) => {
+        if (inquiryDetails.Moving === true) {
+            return <div>
+                <h5>Moving Questions:</h5>
+                <p>New Address: {inquiryDetails.moving_to}</p>
+                <p>Old Address: {inquiryDetails.moving_from}</p>
+                <p>Any Large Items? {inquiryDetails.large_items}</p>
+            </div>
+        } else {
+            return ''
+        }
+    }
+
+    const organizeDeclutterDisplay = (inquiryDetails) => {
+        if (inquiryDetails.Organizing || inquiryDetails.Declutter === true) {
+            return <div>
+                <h5>Organizing/Decluttering Questions:</h5>
+                <p>Wanting to Donate? {donationConversion(inquiryDetails)}</p>
+            </div>
+        } else {
+            return ''
+        }
+    }
+
     //What displays
     // TODO DISPLAY ONLY THE CUSTOMER NAME, THE SERVICES REQUESTED, DATE RECEIVED, COMPLETEION STATUS, NOTES, DETAILS BUTTON
     return (
         <main>
-                <div>
-                    <h1>{inquiryDetails.firstName} {inquiryDetails.lastName}</h1>
-                    {/* gonna work on a function that only displays specific services but not sure I will get to it tonight */}
-                    <h2> 
-                        {serviceConversion(inquiryDetails)} 
-                    </h2>
-                    {/* Do we need this on the inquiries page if they haven't receive service yet? */}
-                    <h3>Date Received: {dateConversion(inquiryDetails.service_on)} </h3>
-                    <h3> {inquiryDetails.completion_status}</h3>
-                    {/* TODO I know the priority is bugged, will work on a fix */}
-                    <h4> {priorityConversion(inquiryDetails)}</h4>
-                    <h2>NOTES: (insert notes here)</h2>
-                    <p>{inquiryDetails.notes}</p>
-                    <button onClick={changeNote}>{noteButton()}</button>
-                    <br />
-                    <h3>Customer Responses to Survey:</h3>
-                    <h5>Cleaning & Basic Questions:</h5>
-                    <p>Number of Bedrooms: {inquiryDetails.Bedrooms}</p>
-                    <p>Number of Bathrooms: {inquiryDetails.Bathrooms}</p>
-                    <p>Number of Additional Rooms: {inquiryDetails.AdditionalRooms}</p>
-                    <p>Number of Doors & Windows: {inquiryDetails.DoorsWindows}</p>
-                    <p>Has Pets? {petConversion(inquiryDetails)}</p>
-                    <p>Hazardous Conditions? {inquiryDetails.HazardousConditions}</p>
-                    <h5>Moving Questions:</h5>
-                    {/* These column names in the database need to have the spaces removed for this to work */}
-                    <p>New Address: {inquiryDetails.moving_to}</p>
-                    <p>Old Address: {inquiryDetails.moving_from}</p>
-                    <p>Any Large Items? {inquiryDetails.large_items}</p>
-                    <h5>Organizing/Decluttering Questions:</h5>
-                    <p>Wanting to Donate? {donationConversion(inquiryDetails)}</p>
-                    <button onClick={returnToInquiries}>Inquiries List</button>
-                </div>
+            <div>
+                <h1>{inquiryDetails.firstName} {inquiryDetails.lastName}</h1>
+                {/* gonna work on a function that only displays specific services but not sure I will get to it tonight */}
+                <h2>
+                    {serviceConversion(inquiryDetails)}
+                </h2>
+                {/* Do we need this on the inquiries page if they haven't receive service yet? */}
+                <h3>Date Received: {dateConversion(inquiryDetails.service_on)} </h3>
+                <h3> {inquiryDetails.completion_status}</h3>
+                {/* TODO I know the priority is bugged, will work on a fix */}
+                <h4> {priorityConversion(inquiryDetails)}</h4>
+                <h2>NOTES:</h2>
+                <p>{inquiryDetails.notes}</p>
+                <button onClick={changeNote}>{noteButton()}</button>
+                <br />
+                <h3>Customer Responses to Survey:</h3>
+                <h5>Basic Questions:</h5>
+                <p>Number of Bedrooms: {inquiryDetails.Bedrooms}</p>
+                <p>Number of Bathrooms: {inquiryDetails.Bathrooms}</p>
+                <p>Number of Additional Rooms: {inquiryDetails.AdditionalRooms}</p>
+                <p>{cleaningDisplay(inquiryDetails)}</p>
+                <p>{movingDisplay(inquiryDetails)}</p>
+                <p>{organizeDeclutterDisplay(inquiryDetails)}</p>
+                <button onClick={returnToInquiries}>Inquiries List</button>
+            </div>
         </main>
 
 
     )
 } // End Inquiries()
-    
-    export default InquiryDetails;
+
+export default InquiryDetails;
