7dd86d06759257f0fd03d47d42bf5dfa471691ef
Everett Butler <everettjamesbutler@gmail.com>
2023-07-06 19:41:00 -0500

    local changes in Inquiries

--- src/components/Inquiries/Inquiries.jsx
+++ src/components/Inquiries/Inquiries.jsx
@@ -57,7 +57,7 @@ function Inquiries() {
                                 {/* I know this should be a different date */}
                                 <h3>{dateConversion(inquiry.service_on)}</h3>
                                 <h3>{inquiry.completion_status}</h3>
-                                <h3>{inquiry.notes}</h3>
+                                <h5>{inquiry.notes}</h5>
                                 <button onClick={(event) => navToInquiryDetails(inquiry)}>Details</button>
                             </div>
                         )
