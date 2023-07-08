4c1418ddd53e84e7ed6d40e8917ca535bcb19722
Sam Gossie <sjg3609@gmail.com>
2023-07-08 16:36:02 -0500

    Working on async GET request, and completed completion and priority routes

--- src/components/CustomerForm/Review.jsx
+++ src/components/CustomerForm/Review.jsx
@@ -9,6 +9,8 @@ function Review() {
     //Code goes here
     const history = useHistory();
     const dispatch = useDispatch();
+    const inquiries = useSelector(store => store.inquiryDetails)
+
 
     const goBack = () => { history.goBack() };
 
@@ -18,13 +20,14 @@ function Review() {
     }
 
     useEffect(() => {
-        dispatch({ type: 'FETCH_INQUIRIES' });
-        // dispatch({ type: 'GET_HISTORY' });
+        // dispatch({ type: 'FETCH_ALL_INFO' });
+        // dispatch({ type: 'FETCH_INQUIRY_DETAILS' });
     }, []);
 
-    const customerDetails = useSelector(store => store.customerReducer);
+    const allUserInfo = useSelector(store => store.allUserInfo);
 
-    console.log('show me the customers!!!!!', customerDetails);
+    console.log('show me the customers!!!!!', allUserInfo);
+    console.log('Show me in the inquiryID!!!!!!!!!!', inquiries)
 
     //What displays
     return (
@@ -32,7 +35,7 @@ function Review() {
             <ProgressBar currentStep={5} />
             <h2 className="h2Headers">Review Your Information:</h2>
             <h3>Customer Information</h3>
-            {
+            {/* {
                 customerDetails.map((customer) =>
                     <div key={customer.id}>
                         <p>{customer.firstName} {customer.lastName}</p>
@@ -40,7 +43,7 @@ function Review() {
                         <p>{customer.street1}</p>
                     </div>
                 )
-            }
+            } */}
 
             <h3>Cleaning Questions</h3>
             <h3>Moving Questions</h3>
