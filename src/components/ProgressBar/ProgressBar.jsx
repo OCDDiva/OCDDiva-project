0033e7f86293d3b9e28bf4650de00120ccc6c3fb
Stephen Vertucci <steve.vertucci@gmail.com>
2023-07-09 14:38:10 -0400

    pulled main changes

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,26 +1,27 @@
 import { Stepper, Step, StepLabel } from '@mui/material';
+import './ProgressBar.css';
 
 const steps = [
-    'Contact',
-    'Cleaning',
-    'Moving',
-    'Organization',
-    'Declutter',
-    'Review',
+  'Contact',
+  'Cleaning',
+  'Moving',
+  'Organization',
+  'Declutter',
+  'Review',
 ];
 
 function ProgressBar({ currentStep }) {
-  
-    return (
-        <Stepper activeStep={currentStep} alternativeLabel>
-          {steps.map((label) => (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-      );
-    }
-    
+  return (
+    <div className="progress-bar-container">
+      <Stepper activeStep={currentStep} alternativeLabel className="progress-bar">
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel className="step-label">{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </div>
+  );
+}
 
-export default ProgressBar; 
+export default ProgressBar;
