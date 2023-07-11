e717107ab5b2cee87b72a73f12aeeae965851ffb
Sam Gossie <sjg3609@gmail.com>
2023-07-11 15:14:15 -0500

    styling updates to the Review Page

--- src/components/CustomerForm/Review.jsx
+++ src/components/CustomerForm/Review.jsx
@@ -11,6 +11,7 @@ function Review() {
     const dispatch = useDispatch();
     const allUserInfo = useSelector(store => store.allUserInfo);
 
+    const [comments, setComments] = useState('');
 
     const goBack = () => { history.goBack() };
 
@@ -30,69 +31,79 @@ function Review() {
 
     //What displays
     return (
-        <div className="customerReview">
+        <div className="reviewPage" >
             <ProgressBar currentStep={5} />
             <center>
                 <h2 className="h2Headers">Review Your Information:</h2>
-                <h3>Customer Information</h3>
-                Name: {allUserInfo.queryResult.rows[0].firstName}{'   '}
-                {allUserInfo.queryResult.rows[0].lastName}{'   '}
                 <br />
-                Address: {allUserInfo.queryResult.rows[0].street1}
+                <div className="customerReview" >
+                    <h3>Customer Information</h3>
+                    Name: {allUserInfo.queryResult.rows[0].firstName}{'    '}
+                    {allUserInfo.queryResult.rows[0].lastName}
+                    <br />
+                    Address: {allUserInfo.queryResult.rows[0].street1}
+                    <br />
+                    {allUserInfo.queryResult.rows[0].street2}
+                    <br />
+                    City: {allUserInfo.queryResult.rows[0].city}
+                    <br />
+                    State: {allUserInfo.queryResult.rows[0].state}
+                    <br />
+                    Zip: {allUserInfo.queryResult.rows[0].zip}
+                    <br />
+                    Phone Number: {allUserInfo.queryResult.rows[0].phone_number}
+                    <br />
+                    Email: {allUserInfo.queryResult.rows[0].email}
+                    <br />
+                    <h3>Cleaning Questions</h3>
+                    Requested Cleaning Service: {allUserInfo.cleaningResult.rows[0].ServiceType}
+                    <br />
+                    Number of Bedrooms: {allUserInfo.cleaningResult.rows[0].Bedrooms}
+                    <br />
+                    Number of Bathrooms: {allUserInfo.cleaningResult.rows[0].Bathrooms}
+                    <br />
+                    Number of Additional Rooms: {allUserInfo.cleaningResult.rows[0].AdditionalRooms}
+                    <br />
+                    {/* TODO add in conditional rendering for this to show if it only has results */}
+                    Any Pets: {allUserInfo.cleaningResult.rows[0].HasPets}
+                    <br />
+                    Hazardous Conditions: {allUserInfo.cleaningResult.rows[0].HazardousConditions}
+                    <br />
+                    <h3>Moving Questions</h3>
+                    Moving To: {allUserInfo.movingResult.rows[0].moving_to}
+                    <br />
+                    Moving From: {allUserInfo.movingResult.rows[0].moving_from}
+                    <br />
+                    Large Items: {allUserInfo.movingResult.rows[0].large_items}
+                    <br />
+                    <h3>Organizing Questions</h3>
+                    Bedrooms: {allUserInfo.orgResult.rows[0].Bedrooms}
+                    <br />
+                    Bathrooms: {allUserInfo.orgResult.rows[0].Bathrooms}
+                    <br />
+                    Additional Rooms: {allUserInfo.orgResult.rows[0].AdditionalRooms}
+                    <br />
+                    Donations: {allUserInfo.orgResult.rows[0].Donations}
+                    <br />
+                    <h3>Decluttering Questions</h3>
+                    Bedrooms: {allUserInfo.decluttResult.rows[0].Bedrooms}
+                    <br />
+                    Bathrooms: {allUserInfo.decluttResult.rows[0].Bathrooms}
+                    <br />
+                    Additional Rooms: {allUserInfo.decluttResult.rows[0].AdditionalRooms}
+                    <br />
+                    Donations: {allUserInfo.decluttResult.rows[0].Donations}
+                </div>
                 <br />
-                {allUserInfo.queryResult.rows[0].street2}
                 <br />
-                City: {allUserInfo.queryResult.rows[0].city}
-                <br />
-                State: {allUserInfo.queryResult.rows[0].state}
-                <br />
-                Zip: {allUserInfo.queryResult.rows[0].zip}
-                <br />
-                Phone Number: {allUserInfo.queryResult.rows[0].phone_number}
-                <br />
-                Email: {allUserInfo.queryResult.rows[0].email}
-                <br />
-                <h3>Cleaning Questions</h3>
-                Requested Cleaning Service: {allUserInfo.cleaningResult.rows[0].ServiceType}
-                <br />
-                Number of Bedrooms: {allUserInfo.cleaningResult.rows[0].Bedrooms}
-                <br />
-                Number of Bathrooms: {allUserInfo.cleaningResult.rows[0].Bathrooms}
-                <br />
-                Number of Additional Rooms: {allUserInfo.cleaningResult.rows[0].AdditionalRooms}
-                <br />
-                {/* TODO add in conditional rendering for this to show if it only has results */}
-                Any Pets: {allUserInfo.cleaningResult.rows[0].HasPets}
-                <br />
-                Hazardous Conditions: {allUserInfo.cleaningResult.rows[0].HazardousConditions}
-                <br />
-                <h3>Moving Questions</h3>
-                Moving To: {allUserInfo.movingResult.rows[0].moving_to}
-                <br />
-                Moving From: {allUserInfo.movingResult.rows[0].moving_from}
-                <br />
-                Large Items: {allUserInfo.movingResult.rows[0].large_items}
-                <br />
-                <h3>Organizing Questions</h3>
-                Bedrooms: {allUserInfo.orgResult.rows[0].Bedrooms}
-                <br />
-                Bathrooms: {allUserInfo.orgResult.rows[0].Bathrooms}
-                <br />
-                Additional Rooms: {allUserInfo.orgResult.rows[0].AdditionalRooms}
-                <br />
-                Donations: {allUserInfo.orgResult.rows[0].Donations}
-                <br />
-                <h3>Decluttering Questions</h3>
-                Bedrooms: {allUserInfo.decluttResult.rows[0].Bedrooms}
-                <br />
-                Bathrooms: {allUserInfo.decluttResult.rows[0].Bathrooms}
-                <br />
-                Additional Rooms: {allUserInfo.decluttResult.rows[0].AdditionalRooms}
-                <br />
-                Donations: {allUserInfo.decluttResult.rows[0].Donations}
-                <br />
-                <h3>Upload some photos of your space!</h3>
-                <input type="photo" />
+                <div className="picsAndComments">
+                    <h3>Upload some photos of your space!</h3>
+                    <input type="photo" />
+                    <br />
+                    <br />
+                    <h4>Leave us any comments that we should know</h4>
+                    <input type="text" value={comments} onChange={(event) => { setComments(event.target.value) }} />
+                </div>
                 <br />
                 <br />
                 <button className="btn" onClick={goBack}> Back </button>
