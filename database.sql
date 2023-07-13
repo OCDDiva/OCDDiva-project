14c9eebe73fe0fcb1e8f9c7e07c0a817641249e4
Sam Gossie <sjg3609@gmail.com>
2023-07-13 10:01:24 -0500

    Updated the database.sql file for the user_media table

--- database.sql
+++ database.sql
@@ -31,6 +31,43 @@ CREATE TABLE "priority" (
 INSERT INTO "priority" ("description")
 VALUES ('High'), ('Medium'), ('Low');
 
+CREATE TABLE "user_inquiries" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES users,
+	"date_received" DATE,
+	"date_requested" DATE,
+	"firstName" varchar(100),
+	"lastName" varchar(250),
+	"street1" varchar(1000),
+	"street2" varchar(1000),
+	"city" varchar(1000),
+	"state" varchar(2),
+	"zip" varchar(5),
+	"phone_number" varchar(11),
+	"email" varchar(50),
+	"priority" INT REFERENCES priority DEFAULT 3,
+	"completion_status" INT REFERENCES completion DEFAULT 1,
+	"comments" VARCHAR(10000)
+);
+
+
+INSERT INTO "user_inquiries" ("id", "user_id", "services_id", "date_received", "date_requested", "firstName", "lastName", "street1", "city", "state", "zip", "phone_number", "email", "priority", "completion_status", "comments")
+VALUES (1, 1, 1, '2023-06-29', '2023-07-01', 'Everett', 'Butler', '123 Vista Wayyy', 'Chiefs-Kingdom', 'MO', '12345', '8165555555', 'chiefsrule@email.com', 1, 5, 'Everett was here');
+
+INSERT INTO "user_inquiries" ("id", "user_id", "services_id", "date_received", "date_requested", "firstName", "lastName", "street1", "city", "state", "zip", "phone_number", "email", "priority", "completion_status", "comments")
+VALUES (2, 2, 2, '2023-06-25', '2023-07-05', 'Miguel', 'Torres', '1234 Bob Ave', 'Chiefs-Kingdom!', 'KS', '54321', '9135555555', 'chiefsrule@gmail.com', 2, 4, 'Miguel was here');
+
+CREATE TABLE "customer" (
+	"id" serial primary key,
+	"user_id" integer REFERENCES users,
+	"inquiries" integer REFERENCES user_inquiries ON DELETE CASCADE,
+	"service_on" date,
+	"notes" VARCHAR(40000)
+);
+
+INSERT INTO "customer" ("id", "user_id", "inquiries", "service_on", "notes")
+VALUES (1, 1, 1, '2023-06-30', 'Everett has notes, yo');
+
 CREATE TABLE "user_media" (
 	id serial primary key,
 	inquiry_id INT references user_inquiries,
@@ -41,7 +78,7 @@ CREATE TABLE "user_media" (
  CREATE TABLE "moving_questions" (
 	id serial primary key,
 	inquiry_id INT references user_inquiries,
-	"moving" BOOLEAN default false, 
+	"moving" BOOLEAN default false,
 	"moving_to" VARCHAR (10000),
 	"moving_from" VARCHAR (10000),
 	"large_items" VARCHAR (10000)
@@ -53,6 +90,7 @@ INSERT INTO "moving_questions" ("moving", "moving_to", "moving_from", "large_ite
 VALUES (false, 'movingAnswer1', 'movingAnswer2', 'movingAnswer3');
 
 
+
 CREATE TABLE "cleaning_questions" (
     id serial primary key,
     inquiry_id INT references user_inquiries,
@@ -62,7 +100,7 @@ CREATE TABLE "cleaning_questions" (
     "Bathrooms" INT,
     "AdditionalRooms" INT,
     "DoorsWindows" INT,
-    "HasPets" VARCHAR(500),
+    "HasPets" varchar(500),
     "HazardousConditions" VARCHAR(10000)
 );
 
@@ -94,54 +132,3 @@ CREATE TABLE "decluttering_questions" (
 
 INSERT INTO "decluttering_questions" ("Declutter", "Bedrooms", "Bathrooms", "AdditionalRooms", "Donation")
 VALUES (true, 3, 2, 1, true);
-
-CREATE TABLE "user_inquiries" (
-	"id" SERIAL PRIMARY KEY,
-	"user_id" INT REFERENCES users,
-	"date_received" DATE,
-	"date_requested" DATE,
-	"firstName" varchar(100),
-	"lastName" varchar(250),
-	"street1" varchar(1000),
-	"street2" varchar(1000),
-	"city" varchar(1000),
-	"state" varchar(2),
-	"zip" varchar(5),
-	"phone_number" varchar(11),
-	"email" varchar(50),
-	"priority" INT REFERENCES priority DEFAULT 3,
-	"completion_status" INT REFERENCES completion DEFAULT 1,
-	"comments" VARCHAR(10000)
-);
-
-
-
-INSERT INTO "user_inquiries" ("id", "user_id", "services_id", "date_received", "date_requested", "firstName", "lastName", "street1", "city", "state", "zip", "phone_number", "email", "priority", "completion_status", "comments")
-VALUES (1, 1, 1, '2023-06-29', '2023-07-01', 'Everett', 'Butler', '123 Vista Wayyy', 'Chiefs-Kingdom', 'MO', '12345', '8165555555', 'chiefsrule@email.com', 1, 5, 'Everett was here');
-
-SELECT
-"services"."description" AS "services_id",
-"dates"."date_submitted" AS "date_received",
-"moving_questions"."id" AS "moving",
-"cleaning_questions"."id" AS "cleaning",
-"organizing_questions"."id" AS "organizing",
-"decluttering_questions"."id" AS "declutting"
-FROM "user_inquiries"
-JOIN "services" ON "user_inquiries"."services_id" = "services"."id"
-JOIN "dates" ON "user_inquiries"."date_received" = "dates"."id"
-JOIN "moving_questions" ON "user_inquiries"."moving" = "moving_questions"."id"
-JOIN "cleaning_questions" ON "user_inquiries"."cleaning" = "cleaning_questions"."id"
-JOIN "organizing_questions" ON "user_inquiries"."organizing" = "organizing_questions"."id"
-JOIN "decluttering_questions" ON "user_inquiries"."declutting" = "decluttering_questions"."id";
-
-
-CREATE TABLE "customer" (
-	"id" serial primary key,
-	"user_id" integer REFERENCES users,
-	"inquiries" integer REFERENCES user_inquiries ON DELETE CASCADE,
-	"service_on" date,
-	"notes" VARCHAR(40000)
-);
-
-INSERT INTO "customer" ("id", "user_id", "inquiries", "service_on", "notes")
-VALUES (1, 1, 1, '2023-06-30', 'Everett has notes, yo');
