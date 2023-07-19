011854ef22d52db6c038d72f5174ce4c2c69f996
Sam Gossie <sjg3609@gmail.com>
2023-07-18 19:05:10 -0500

    Cleaning up some code on pages

--- server/routes/userHistory.router.js
+++ server/routes/userHistory.router.js
@@ -3,14 +3,15 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/userHistory', (req, res) => {
+    const customerId = req.user.id;
     console.log('In userHistory Router');
     const queryText = `SELECT * FROM "user_inquiries"
     JOIN "moving_questions" ON "moving_questions"."inquiry_id" = "user_inquiries"."id"
     JOIN "cleaning_questions" ON "cleaning_questions"."inquiry_id" = "user_inquiries"."id"
     JOIN "organizing_questions" ON "organizing_questions"."inquiry_id" = "user_inquiries"."id"
     JOIN "decluttering_questions" ON  "decluttering_questions"."inquiry_id" = "user_inquiries"."id"
-    WHERE "user_inquiries"."user_id" = 1;`;
-    pool.query(queryText).then((result) => {
+    WHERE "user_inquiries"."user_id" = $1;`;
+    pool.query(queryText, [customerId]).then((result) => {
         console.log('Checking results', result.rows);
         res.send(result.rows);
     })
